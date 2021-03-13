22109b9eb25a16056d905fc9b0753721cd6c17f9
Olimpia Ursu <olimpia_denisa.ursu@stud.acs.upb.ro>
2021-03-13 19:14:47 +0200

    result page

--- src/pages/results.js
+++ src/pages/results.js
@@ -1,9 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Layout from '../layout/layout';
+import styles from '../styling/results_styles.module.scss';
+import { results } from '../static_data/results';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Results = () => {
   document.title = 'BroBots - Results';
-  return <Layout>Results</Layout>;
+  return (
+    <Layout>
+      <div className={styles.container}>
+        {results.map((event, id) => (
+          <div key={id} className={styles.card}>
+            <div className={styles.info}>
+              <Link to={`/events/${event.key}`}>
+                <h4>{event.title}</h4>
+              </Link>
+
+              <p className={styles.location}>{event.location}</p>
+              <p>{event.description}</p>
+            </div>
+            <div className={styles.carousel}>
+              <Carousel pause='false' touch>
+                {event.images.length > 0 &&
+                  event.images.map((image, key) => (
+                    <Carousel.Item interval={2000} key={key}>
+                      <img
+                        className='d-block w-100'
+                        src={image}
+                        alt='First slide'
+                      />
+                    </Carousel.Item>
+                  ))}
+              </Carousel>
+            </div>
+
+            <p className={styles.date}>{event.date}</p>
+          </div>
+        ))}
+      </div>
+    </Layout>
+  );
 };
 
 export default Results;
---
+++ src/static_data/results.js
@@ -0,0 +1,14 @@
+//import promovare_2020 from '../assets/events/promovare_2020.png';
+import poza1 from '../assets/results/pozatimisoara2020.jpg';
+import diploma from '../assets/results/diploma2020.jpg';
+
+export const results = [
+  {
+    title: 'Regionala Timișoara 2020',
+    description:
+      'Sâmbătă şi duminică la Timişoara am scris o nouă pagină din povestea BroBots. Etapa regională a competiţiei "First Tech Challenge" ne-a adus în aceste două zile un amalgam de sentimente şi trăiri: bucurie, emoţie, entuziasm, optimism, speranţă şi în final bucurie trăită la maximă intensitate. BroBots Team s-a aflat, ca şi anul trecut, printre premianţii competiţiei, activitatea echipei fiind recompensată cu Premiul II Connect Award, premiu care ne asigură şi calificarea la etapa finală a competiţiei care va avea loc la Cluj-Napoca. Mulţumim sponsorilor, foştilor elevi ai colegiului şi părinţilor care au făcut posibilă participarea noastră în această competiţie de robotică!',
+    images: [diploma, poza1],
+    location: 'Sala lui Traian, Timișoara',
+    date: '21 - 22 Feb'
+  }
+];
